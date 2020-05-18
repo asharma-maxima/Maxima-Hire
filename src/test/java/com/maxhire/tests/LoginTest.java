b433b6b8b98993c239b81aba43e4e164c03f8d0c
Aakash <asharma@maximaconsulting.net>
2020-05-18 05:11:36 -0700

    added new utility file - BrowserUtil.java updated BaseTest.java updated LoginTest.java

--- src/test/java/com/maxhire/tests/LoginTest.java
+++ src/test/java/com/maxhire/tests/LoginTest.java
@@ -9,10 +9,7 @@ public class LoginTest extends BaseTest{
 	@Test	
 	public void login() throws InterruptedException {
 		LoginPage loginPage = new LoginPage(driver);
-		boolean isPresent = loginPage.getMail_add().isDisplayed();
-		System.out.println("email address field is present "+isPresent);
-		loginPage.getMail_add().sendKeys(username);
-		loginPage.getPassword().sendKeys(password);
-		loginPage.getLogin_btn().click();
+		loginPage.login(username, password);
+		loginPage.verifyLoginPageTitle("maxhire");
 	}
 }
